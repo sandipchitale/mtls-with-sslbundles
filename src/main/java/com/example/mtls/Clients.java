eeae8271a3830719e3ce3f6de3fa8a91efb1f3fb
sandipchitale <sandipchitale@gmail.com>
2023-12-03 20:17:44 -0800

    Also with 127.0.0.1 trust for RestClient and RestTemplate.

--- src/main/java/com/example/mtls/Clients.java
+++ src/main/java/com/example/mtls/Clients.java
@@ -1,9 +1,17 @@
 package com.example.mtls;
 
 import io.netty.handler.ssl.SslContext;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.io.HttpClientConnectionManager;
+import org.apache.hc.client5.http.ssl.DefaultHostnameVerifier;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
 import org.springframework.boot.ssl.SslBundle;
 import org.springframework.boot.ssl.SslBundles;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Component;
@@ -11,8 +19,10 @@ import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLException;
+import javax.net.ssl.TrustManager;
 import java.net.http.HttpClient;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
@@ -24,7 +34,9 @@ import java.security.NoSuchAlgorithmException;
 class Clients {
 
     private final RestClient restClient;
+    private final RestClient restClientLoopback;
     private final RestTemplate restTemplate;
+    private final RestTemplate restTemplateLoopback;
     private final WebClient webClient;
 
     Clients(SslBundles sslBundles,
@@ -42,11 +54,56 @@ class Clients {
         this.restClient = restClientBuilder.requestFactory(requestFactory).build();
         this.restTemplate = restTemplateBuilder.requestFactory(() -> requestFactory).build();
 
+        ClientHttpRequestFactory loopbackClientHttpRequestFactory = getClientHttpRequestFactory(testBundle);
+        this.restClientLoopback = restClientBuilder.requestFactory(loopbackClientHttpRequestFactory).build();
+        this.restTemplateLoopback = restTemplateBuilder.requestFactory(() -> loopbackClientHttpRequestFactory).build();
+
         SslContext reactorSslContext = sslContextFactory.buildReactorSslContext(testBundle, caCerts);
         reactor.netty.http.client.HttpClient reactorHttpClient = reactor.netty.http.client.HttpClient
             .create()
             .secure(sslContextSpec -> sslContextSpec.sslContext(reactorSslContext));
         this.webClient = webClientBuilder.clientConnector(new ReactorClientHttpConnector(reactorHttpClient)).build();
+    }
+
+    private static class LoopbackIPHostnameVerifier implements HostnameVerifier {
+
+        private final DefaultHostnameVerifier defaultHostnameVerifier = new DefaultHostnameVerifier();
+
+        @Override
+        public boolean verify(String hostname, javax.net.ssl.SSLSession sslSession) {
+            if ("127.0.0.1".equals(hostname)) {
+                return true;
+            }
+            return defaultHostnameVerifier.verify(hostname, sslSession);
+        }
+    }
+
+    private ClientHttpRequestFactory getClientHttpRequestFactory(SslBundle sslBundle) throws NoSuchAlgorithmException, KeyManagementException {
+        // Build request factory with SSLContext from SslBundle but with a custom LoopbackIPHostnameVerifier
+        SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(
+            sslBundle
+                .getManagers()
+                .createSslContext(sslBundle.getProtocol()),
+            new LoopbackIPHostnameVerifier());
+
+        HttpClientConnectionManager httpClientConnectionManager = PoolingHttpClientConnectionManagerBuilder
+            .create()
+            .setSSLSocketFactory(sslConnectionSocketFactory)
+            .build();
+        CloseableHttpClient closeableHttpClient = HttpClients.custom()
+            .setConnectionManager(httpClientConnectionManager)
+            .evictExpiredConnections()
+            .build();
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(closeableHttpClient);
+        return requestFactory;
+    }
+
+    public RestClient getRestClientLoopback() {
+        return restClientLoopback;
+    }
+
+    public RestTemplate getRestTemplateLoopback() {
+        return restTemplateLoopback;
     }
 
     public RestClient getRestClient() {
@@ -61,3 +118,5 @@ class Clients {
         return webClient;
     }
 }
+
+
--- src/main/java/com/example/mtls/MTlsApplication.java
+++ src/main/java/com/example/mtls/MTlsApplication.java
@@ -10,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
 
 import java.util.Objects;
 
@@ -33,18 +34,54 @@ public class MTlsApplication {
     @Bean
     CommandLineRunner clr(Clients client) {
         return (String... args) -> {
+
+            // RestClient
+            LOGGER.info("RestClient loopback");
             ResponseEntity<String> localhost = client.getRestClient().get().uri("https://localhost:8443").retrieve().toEntity(String.class);
             LOGGER.info("RestClient: Got {} from localhost", localhost.getStatusCode());
+
+            LOGGER.info("RestClient 127.0.0.1 without loopback trust");
+            try {
+                ResponseEntity<String> loopback = client.getRestClient().get().uri("https://127.0.0.1:8443").retrieve().toEntity(String.class);
+                LOGGER.info("RestClient 127.0.0.1: Got {} from 127.0.0.1", localhost.getStatusCode());
+            } catch (Exception e) {
+                LOGGER.error("Expected error for 127.0.0.1: " + e.getMessage());
+            }
+
+            LOGGER.info("RestClient 127.0.0.1 with loopback trust");
+            ResponseEntity<String> loopback = client.getRestClientLoopback().get().uri("https://127.0.0.1:8443").retrieve().toEntity(String.class);
+            LOGGER.info("RestClient 127.0.0.1 with loopback trust: Got {} from 127.0.0.1", localhost.getStatusCode());
+
+            LOGGER.info("RestClient example.com");
             ResponseEntity<String> exampleCom = client.getRestClient().get().uri("https://example.com").retrieve().toEntity(String.class);
             LOGGER.info("RestClient: Got {} from example.com", exampleCom.getStatusCode());
 
+            // RestTemplate
+            LOGGER.info("RestTemplate localhost");
             localhost = client.getRestTemplate().getForEntity("https://localhost:8443", String.class);
             LOGGER.info("RestTemplate: Got {} from localhost", localhost.getStatusCode());
+
+            LOGGER.info("RestTemplate 127.0.0.1 without loopback trust");
+            try {
+                loopback = client.getRestTemplate().getForEntity("https://127.0.0.1:8443", String.class);
+                LOGGER.info("RestTemplate: Got {} from 127.0.0.1", loopback.getStatusCode());
+            } catch (Exception e) {
+                LOGGER.error("Expected error for 127.0.0.1: " + e.getMessage());
+            }
+
+            LOGGER.info("RestTemplate 127.0.0.1 with loopback trust");
+            loopback = client.getRestTemplateLoopback().getForEntity("https://127.0.0.1:8443", String.class);
+            LOGGER.info("RestTemplate 127.0.0.1 with loopback trust: Got {} from 127.0.0.1", loopback.getStatusCode());
+
+            LOGGER.info("RestTemplate example.com");
             exampleCom = client.getRestTemplate().getForEntity("https://example.com", String.class);
             LOGGER.info("RestTemplate: Got {} from example.com", exampleCom.getStatusCode());
 
+            LOGGER.info("WebClient localhost");
             localhost = client.getWebClient().get().uri("https://localhost:8443").retrieve().toEntity(String.class).block();
             LOGGER.info("WebClient: Got {} from localhost", Objects.requireNonNull(localhost).getStatusCode());
+
+            LOGGER.info("WebClient example.com");
             exampleCom = client.getWebClient().get().uri("https://example.com").retrieve().toEntity(String.class).block();
             LOGGER.info("WebClient: Got {} from example.com", Objects.requireNonNull(exampleCom).getStatusCode());
         };
